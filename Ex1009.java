0e1c86281ad437cea964a0b8bdd608e822dbca68
Alberto Figuerêdo <34289440+BunnyCoder@users.noreply.github.com>
2017-12-08 16:15:02 -0200

    Mudei a impressão do printf
    o %n estava dando erro no URI, e não havia necessidade de 2 scanners, apesar de em alguns casos ter conflito de leituras com tipos diferentes de variáveis.

--- Ex1009.java
+++ Ex1009.java
@@ -10,11 +10,12 @@ import java.util.Scanner;
 public class Ex1009 {
     public static void main(String[] args) {
         Scanner hp = new Scanner(System.in);
-        Scanner epson = new Scanner(System.in);
+        
         String nome = epson.nextLine();
         double salFix = hp.nextDouble();
         double totVend = hp.nextDouble();
-        double sal = salFix + totVend*0.15;
-        System.out.printf("TOTAL = R$ %.2f%n", sal);
+        
+        double totSal = salFix + totVend*0.15;
+        System.out.printf("TOTAL = R$ %.2f\n", totSal);
     }
 }
